cce2ddc3aaada2e82192c64a0aeba25c5b81ebb6
toyamarinyon <toyamarinyon@gmail.com>
2025-04-02 09:32:56 +0900

    Add website landing page component

---
+++ apps/playground/app/website/page.tsx
@@ -0,0 +1,18 @@
+export default function () {
+	return (
+		<div className="bg-black-900 h-screen text-white-800 px-[32px] py-[10px]">
+			<div className="ml-[34px] mt-[34px]">
+				<p className="font-accent text-[38px] font-[700] text-white-800 tracking-wider">
+					AI power without the complexity{" "}
+				</p>
+				<p className="text-white-400 text-[15px]/[16px]">
+					Harness global LLMs, build custom AI solutions in minutes not months,
+					<br />
+					and seamlessly integrate with your existing workflow—the sophisticated
+					<br />
+					AI platform designed for non-technical teams.
+				</p>
+			</div>
+		</div>
+	);
+}
